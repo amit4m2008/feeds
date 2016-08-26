5b6b2db48876f85b619f11e07dd8e29e0a682672
amit4m2008 <amit4m2008@gmail.com>
2016-08-26 14:17:27 +0530

    Changes: 1 - Feed Items descending order sorting added. 2 - Helpers added for feed show page. 3 - Footer css changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def sanitize_item_description(text)
+    return ActionView::Base.full_sanitizer.sanitize(text).squish rescue ""
+  end
+
+  def published_date(date_string)
+    return date_string.to_datetime.strftime("%a, %d %b %Y %H:%M:%S") rescue ""
+  end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -5,8 +5,16 @@ class Feed < ActiveRecord::Base
   validates_presence_of :feed_url
 
   def parse_rss_feeds
+    feed_hash = {}
     doc = Nokogiri::HTML(open(self.feed_url))
-    items = doc.css("rss channel item")
-    return items
+    items = doc.css("item")
+
+    items.each do |item|
+      pubdate = item.at('pubdate').text.to_datetime.to_i.to_s
+      feed_hash[pubdate] = item
+    end
+    feed_hash = feed_hash.sort.reverse.to_h.values
+    return feed_hash
   end
+
 end
